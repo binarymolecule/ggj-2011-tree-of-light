f5da7c2d6d5c51a1ae49711d6ac0910229f3d2ae
Marcel Jackwerth <marcel@northdocks.com>
2011-01-29 10:13:52 +0000

    Added GOs

---
+++ lightseeds/lightseeds/lightseeds/lightseeds/GameObjects/Tree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace lightseeds.GameObjects
+{
+    class Tree
+    {
+        public Vector2 position;
+
+        // gameplay properties
+        public float growthTime;
+        public float growthAmount = 0f;
+        public int price;
+    }
+}
